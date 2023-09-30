c559485d08a2cdb979b4ffddb5c083934464db80
Cueva Tomas Santiago <tomassantiagocueva@gmail.com>
2023-09-30 07:46:07 -0300

    f(Training_cards):ahora es posible agregar bloques, agregar ejercicios a cada bloque y aumentar las repetiones junto con sus kilos respectivos en cada bloque.

--- training_app/static/js/TrainingSession.js
+++ training_app/static/js/TrainingSession.js
@@ -1,11 +1,34 @@
 const addBlock = document.getElementById('add-block');
 const training = document.getElementById('training');
 const metaCantBlocks = document.getElementById('meta-cant-blocks');
-const metaCantExercises = document.getElementById('meta-cant-exercises');
+const metaCantExercises = document.getElementById('meta-cant-excercise');
 
-function addEventIncrementDecrementValue(input, add, sub, id) {
-    const repsInputId = document.querySelector(`#reps-inputs-${id}`);
-    const kgsInputId = document.querySelector(`#kgs-inputs-${id}`);
+
+function addEventIncrementDecrementValue(idBlock) {
+    const input = document.getElementById(`total-reps-${idBlock}`);
+    const add = document.getElementById(`add-reps-exercise-${idBlock}`);
+    const sub = document.getElementById(`sub-reps-exercise-${idBlock}`);
+
+    add.addEventListener('click', () => {
+        let currentValue = parseInt(input.value, 10);
+        input.value = currentValue + 1;
+    });
+
+    sub.addEventListener('click', () => {
+        let currentValue = parseInt(input.value, 10);
+        if (currentValue > 1) {
+            input.value = currentValue - 1;
+        }
+    });
+}
+
+function manipulateInputs(idBlock, idExercise) {
+    const repsInputId = document.querySelector(`#reps-inputs-${idExercise}`);
+    const kgsInputId = document.querySelector(`#kgs-inputs-${idExercise}`);
+    const add = document.getElementById(`add-reps-exercise-${idBlock}`);
+    const sub = document.getElementById(`sub-reps-exercise-${idBlock}`);
+    let lenghtInputs = document.querySelectorAll(`#reps-inputs-${idExercise} input`).length;
+    console.log(lenghtInputs);
 
     function createInput(type, inputId, inputName) {
         const inputElement = document.createElement('input');
@@ -17,24 +40,26 @@ function addEventIncrementDecrementValue(input, add, sub, id) {
     }
 
     add.addEventListener('click', () => {
-        let currentValue = parseInt(input.value, 10);
-        input.value = currentValue + 1;
-        repsInputId.appendChild(createInput('number', `reps_${id}`, `reps_${id}`));
-        kgsInputId.appendChild(createInput('number', `kgs_${id}`, `kgs_${id}`));
+        repsInputId.appendChild(createInput('number', `reps_${idExercise}`, `reps_${idExercise}`));
+        kgsInputId.appendChild(createInput('number', `kgs_${idExercise}`, `kgs_${idExercise}`));
+        lenghtInputs ++;
+        console.log(lenghtInputs);
     });
 
     sub.addEventListener('click', () => {
-        let currentValue = parseInt(input.value, 10);
-        if (currentValue > 1) {
-            input.value = currentValue - 1;
+        console.log(lenghtInputs);
+        if (lenghtInputs > 1) {
             repsInputId.removeChild(repsInputId.lastChild);
             kgsInputId.removeChild(kgsInputId.lastChild);
+            lenghtInputs --;
+            console.log(lenghtInputs);
         }
     });
 }
 
 function blockExcercise (id) {
     console.log('blockExcercise');
+    console.log(id);
     const block = document.createElement('article');
     block.classList.add('block-exercises');
     block.innerHTML = `
@@ -69,18 +94,18 @@ function trainingCard (idBlock, idExercise) {
     card.classList.add('card');
     card.innerHTML = `
         <div class="card-header">
-            <input type="text" id="name_exercise_${idBlock}" name="name_exercise_${idBlock}" class="form-control" placeholder="Bloque">
+            <input type="text" id="name_block_${idBlock}" name="name_block_${idBlock}" class="form-control" placeholder="Bloque">
             <span>
                 <i class="bi bi-arrow-right"></i>
             </span>
             <span>Repeticiones</span>
             <div class="card-header__reps-conf">
                 <span href="#" id=add-reps-exercise-${idBlock}><i class="bi bi-plus-lg"></i></span>
-                <input type="text" id="exercise_${idBlock}" name="exercise_${idBlock}" class="form-control" readonly value="1">
+                <input type="text" id="total-reps-${idBlock}" name="total-reps-${idBlock}" class="form-control" readonly value="1">
                 <span href="#" id=sub-reps-exercise-${idBlock}><i class="bi bi-dash-lg"></i></span>
             </div>
         </div>
-        <div id="exercises-layout">
+        <div id="exercises-layout-${idBlock}" class="exercises-layout">
             <article class="block-exercises">
                 <input type="text" placeholder="Ejercicio" id="excercise-${idExercise}" name="excercise-${idExercise}">
                 <aside class="exercises-layout__kgs-reps">
@@ -102,41 +127,40 @@ function trainingCard (idBlock, idExercise) {
                     <span>Anotar percepción del esfuerzo</span>
                 </div>
             </article>
-            <span id="add-excersice-${id}"><i class="bi bi-plus-lg"></i><span>Agregar ejercicio</span></span>
+            <span id="add-excersice-${idBlock}"><i class="bi bi-plus-lg"></i><span>Agregar ejercicio</span></span>
         </div>
     `;
 
     return card;
 }
 
-function addEventAddExercise(idExercise) {
-    const addExercise = document.getElementById(`add-excersice-${idExercise}`);
-    const block = document.getElementById(`exercises-layout`);
+function addEventAddExercise() {
+    let idBlock = metaCantBlocks.value;
+    const addExercise = document.getElementById(`add-excersice-${idBlock}`);
+    const block = document.getElementById(`exercises-layout-${idBlock}`);
     addExercise.addEventListener('click', () => {
-        const exercise = blockExcercise(id);
+        metaCantExercises.value = parseInt(metaCantExercises.value, 10) + 1;
+        let idExercise = metaCantExercises.value;
+        const exercise = blockExcercise(idExercise);
         block.insertBefore(exercise, addExercise);
-        const input = document.getElementById(`exercise_${idExercise}`);
-        const add = document.getElementById(`add-reps-exercise-${idExercise}`);
-        const sub = document.getElementById(`sub-reps-exercise-${idExercise}`);
-        addEventIncrementDecrementValue(input, add, sub, id);
+        manipulateInputs(idBlock, idExercise);
     });
 }
 
 addBlock.addEventListener('click', () => {
     metaCantBlocks.value = parseInt(metaCantBlocks.value, 10) + 1;
+    console.log(metaCantBlocks.value);
     metaCantExercises.value = parseInt(metaCantExercises.value, 10) + 1;
+    console.log(metaCantExercises.value);
     
-    let idBLock = blockExcercise.value;
-    let idExercise = trainingCard.value;
-    const card = trainingCard(idBLock, idExercise);
+    let idBlock = metaCantBlocks.value;
+    let idExercise = metaCantExercises.value;
+    const card = trainingCard(idBlock, idExercise);
     
     training.insertBefore(card, addBlock);
     
-    const input = document.getElementById(`exercise_${idBLock}`);
-    const add = document.getElementById(`add-reps-exercise-${idBLock}`);
-    const sub = document.getElementById(`sub-reps-exercise-${idBLock}`);
-    
-    addEventIncrementDecrementValue(input, add, sub, idBLock);
-    addEventAddExercise(idExercise);
+    addEventIncrementDecrementValue(idBlock);
+    manipulateInputs(idBlock, idExercise);
+    addEventAddExercise();
 });
 
